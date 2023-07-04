08709845d81b44b5528e0a287dd8c6c65844bb19
Zach <zachlandis91@gmail.com>
2023-07-04 10:16:51 -0600

    Fix syntax error configured client-side routing

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,14 @@
 Rails.application.routes.draw do
-  
   get '/hello', to: 'application#hello_world'
 
   get '*path',
-    to: 'fallback#index'
-    constraints: ->(req) { !req.xhr? && req.format.html? }
-  
-  
+      to: 'fallback#index',
+      constraints: ->(req) { !req.xhr? && req.format.html? }
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
 end
+
+
